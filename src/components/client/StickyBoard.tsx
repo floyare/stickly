09b9334a37ea2bc4ee980f1fd78eba287a84bc6e
floyare <neptuneeex@gmail.com>
2024-01-03 16:40:54 +0100

    FCP Fix, font

--- src/components/client/StickyBoard.tsx
+++ src/components/client/StickyBoard.tsx
@@ -3,7 +3,7 @@ import Styles from "../../styles/StickyBoard.module.scss"
 const StickyBoard = () => {
     return (
         <section className={Styles.sticky__board}>
-            
+            <h1>Sticky Board</h1>
         </section>
     );
 }
