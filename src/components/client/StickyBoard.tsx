e8958b51f55c80592a4109bc828fc28594515025
floyare <neptuneeex@gmail.com>
2024-01-03 16:32:11 +0100

    First upload

---
+++ src/components/client/StickyBoard.tsx
@@ -0,0 +1,11 @@
+import Styles from "../../styles/StickyBoard.module.scss"
+
+const StickyBoard = () => {
+    return (
+        <section className={Styles.sticky__board}>
+            
+        </section>
+    );
+}
+
+export default StickyBoard;
