378a76f5c511104cef06a3d419e10e0be8b53c7d
floyare <neptuneeex@gmail.com>
2024-01-03 17:40:39 +0100

    change to global styling

--- src/components/client/StickyBoard.tsx
+++ src/components/client/StickyBoard.tsx
@@ -1,9 +1,12 @@
-import Styles from "../../styles/StickyBoard.module.scss"
+//import Styles from "../../styles/StickyBoard.module.scss"
+import "../../styles/StickyBoard.scss";
+import StickyNote from "./StickyNote";
 
 const StickyBoard = () => {
     return (
-        <section className={Styles.sticky__board}>
+        <section className="sticky__board"/*className={Styles.sticky__board}*/>
             <h1>Sticky Board</h1>
+            <StickyNote />
         </section>
     );
 }
---
+++ src/components/client/StickyNote.tsx
@@ -0,0 +1,9 @@
+const StickyNote = () => {
+    return (
+        <div className="sticky__note">
+            <p>Example note</p>
+        </div>
+    );
+}
+ 
+export default StickyNote;
