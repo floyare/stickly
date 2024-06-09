67f0152d5e50ac7412efcecfb68b4d2c05eff9bc
floyare <neptuneeex@gmail.com>
2024-06-09 10:26:05 +0200

    feat: :sparkles: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+    site: "https://stickly.pages.dev/",
+    integrations: [react(), sitemap()],
     output: "server",
     adapter: cloudflare()
 });
